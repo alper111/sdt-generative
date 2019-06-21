13946f8c213588eb4cb136dae9a27d40edfd0f87
alper111 <ahmetoglu.alper@gmail.com>
2019-06-21 12:39:26 +0300

    mad test

--- mad.sh
+++ mad.sh
@@ -2,23 +2,23 @@
 
 python3 -u train_madgan.py \
 -g_model    "conv" \
--g_layers   16 8 1 \
--g_num      5 \
+-g_layers   128 64 1 \
+-g_num      4 \
 -g_norm     "batch_norm" \
 -d_model    "conv" \
--d_layers   8 16 32 \
+-d_layers   64 128 256 \
 -d_norm     "batch_norm" \
--input_shape 32 4 4 \
+-input_shape 256 4 4 \
 -z_dim      100 \
 -batch_size 128 \
 -test_size  10000 \
--lr         1e-4 \
+-lr         2e-4 \
 -lr_decay   1.0 \
 -lr_step    1 \
 -wasserstein 0 \
--epoch      50 \
+-epoch      250 \
 -out        "out/MADGAN/test" \
--seed       1 \
+-seed       2019 \
 -device     "cuda:0" \
 -dataset    "mnist" \
 -c_iter     1 \
